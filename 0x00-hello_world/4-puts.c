ee2865c9715274c68aa7120090058f9f8cdbd8e0
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-15 18:45:36 -0700

    file 4-puts.c content change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * this program uses puts to output a text
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }

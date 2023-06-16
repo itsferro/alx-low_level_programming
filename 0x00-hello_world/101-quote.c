91da31d4ebc40db33785d26315732877f75e9a91
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-15 19:50:46 -0700

    file 101-quote.c content change

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,8 +7,6 @@
  */
 int main(void)
 {
-	char message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	size_t length = sizeof(message);
-	write(2, message, length);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
 	return (1);
 }

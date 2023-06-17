6a5c85eac73ed8f99545a413e2bdc7713a8cf052
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-16 18:49:36 -0700

    file content change

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,10 +8,13 @@
  */
 int main(void)
 {
-	int n;
+	char n;
 
-	for (n = 0; n < 9; n++)
-		printf("%d", n);
-	putchar("\n");
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
 	return (0);
 }

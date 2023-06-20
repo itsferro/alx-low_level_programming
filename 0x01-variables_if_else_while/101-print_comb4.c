be2cdcd32a746c218818fb4b0f9b978ce59e9a47
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-20 04:57:19 -0700

    Task 11. The success combination in business is: Do what you do better... and: do more of what you do... answered

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a serie of 3digit numbers with commas
+  *
+  * Return: Always (Success);
+  * */
+int main(void)
+{
+	int n;
+	int n1;
+	int n2;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (n1 = (1 + n); n1 <= '9'; n1++)
+		{
+			for (n2 = (1 + n1); n2 <= '9'; n2++)
+			{
+				putchar(n);
+				putchar(n1);
+				putchar(n2);
+				if (n != '7' || n1 != '8' || n2 != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

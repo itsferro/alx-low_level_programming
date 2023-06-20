4f5fe635b6e586e4c346cf02c2eee7bff5689095
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-20 16:38:35 -0700

    Task 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game answered

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * print_alphabet - prints alphapet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int alphabet;
+
+	for (alphabet == 'a'; alphabet == 'z'; plphabet++)
+	{
+		_putchar(plphabet);
+	}
+	_putchar('\n');
+
+	return
+}

5041da615717aef1da1cd6b7491551ebf1500bad
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-20 05:24:19 -0700

    file 102-print_comb5.c content change

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -27,7 +27,7 @@ int main(void)
 					putchar(' ');
 					putchar(t);
 					putchar(o);
-					if ((tens != '9' && ones != '8') && (t != '9' && o != '9'))
+					if (tens != '9' && ones != '8' && t != '9' && o != '9')
 					{
 						putchar(',');
 						putchar(' ');

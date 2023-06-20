cfb613d49e5cfa449d1f06c41d12b0d8b17e26e1
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-20 16:56:38 -0700

    file 1-alphabet.c and main.h content change 5

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,14 @@
+#ifndef _myfile_h
+#define _myfile_h
+
+/**
+ * _putchar - prints a char
+ */
 int _putchar(char x);
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ */
 void print_alphabet(void);
+
+#endif

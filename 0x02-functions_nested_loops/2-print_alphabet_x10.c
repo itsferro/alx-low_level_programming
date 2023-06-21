99fbc9d6ff09d0467f42923a5be78bb9d6cf15f5
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-20 17:11:39 -0700

    Task 2. 10 x alphabet answered

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphapet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int i = 1;
+
+
+	while (i <= 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,9 @@ int _putchar(char x);
  */
 void print_alphabet(void);
 
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void);
+
 #endif

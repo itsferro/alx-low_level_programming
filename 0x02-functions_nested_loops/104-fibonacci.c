39eddf50b36d9c5b47b4f233913d9435c37572b5
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-21 10:57:02 -0700

    file 104-fibonacci.c answered

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	long int pn1, pn2, vh;
+	int i;
+
+	pn2 = 0;
+	pn1 = 1;
+
+	for (i = 0; i < 98; i++)
+	{
+		vh = pn1;
+		pn1 = pn1 + pn2;
+		pn2 = vh;
+		if (i < 97)
+		{
+			printf("%ld, ", pn1);
+		}
+		else
+		{
+			printf("%ld", pn1);
+		}
+	}
+	printf("\n");
+	return (0);
+}

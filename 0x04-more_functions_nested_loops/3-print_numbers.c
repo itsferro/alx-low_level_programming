19298d3ff33e691d604e2fc0ce60d9959628873c
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-22 18:16:45 -0700

    Task 3. The numbers speak for themselves answered

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9.
+ *
+ * Return: void;
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		_putchar('9');
+		i++
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN
 #define MAIN
 
+void _putchar(void);
+
 int _isupper(int c);
 
 int _isdigit(int c);
 
 int mul(int a, int b);
+
+void print_numbers(void);
 
 #endif

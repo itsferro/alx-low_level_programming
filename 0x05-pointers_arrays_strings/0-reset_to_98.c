a4ac628738ce6fc5c1204f0fd8e2addc0c3a437c
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-26 16:28:57 -0700

    Task 0. 98 Battery st. fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - takes a pointer to an int as parameter
+ *	and updates the value it points to to 98.
  *
- * @*n: a pointer to an integer.
+ * @n: a pointer to an integer.
  *
  * Return: void
  */

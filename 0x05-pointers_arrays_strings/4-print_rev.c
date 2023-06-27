c17d093954781779ce19a3d7dfbf10651c19f824
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-26 17:08:40 -0700

    file 4-print_rev.c content change

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,20 +3,20 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout.
  *
- * @str: a pointer to a character
+ * @s: a pointer to a character
  *
  * Return: void
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
-	while (*str != '\0')
+	while (*s != '\0')
 	{
-		str++;
+		s++;
 	}
-	while (*str == '\0')
+	while (*s == '\0')
 	{
-		_putchar(*str);
-		str--;
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }

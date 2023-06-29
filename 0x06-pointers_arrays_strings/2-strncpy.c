e1444801cbacd2dd001d017b7954f2c5b62ada94
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-29 13:31:14 -0700

    Task 2. strncpy answered

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+/* #include <stdio.h> */
 
 /**
  * _strncpy - copies a string.
@@ -13,20 +14,33 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int dest_length, i;
 
+	dest_length = 0;
+	/* printf("\n%p the address of dest before counting\n", dest); */
 	while (*dest != '\0')
 	{
 		dest_length++;
 		dest++;
+		/**
+		 * printf("%d the value of the last dest_length counter\n", dest_length);
+		 * printf("%p the address of dest throgh counting\n", dest);
+		 */
 	}
+	/**
+	 * printf("\n%d the value of the last dest element\n", *dest);
+	 * printf("\n%p the address of dest after counting\n", dest);
+	 */
 
 	dest -= dest_length;
+	/* printf("\n%p the address of dest after dest -= dest_length;\n", dest); */
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
-	for (; i < dest_length; i++)
-		dest[i] = '\0';
+	/*
+	 * for (; i < dest_length; i++)
+	 * dest[i] = '\0';
+	 */
 
-	return dest;
+	return (dest);
 }

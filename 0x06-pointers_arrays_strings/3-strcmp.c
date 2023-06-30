6a40ce95edcc2fbbf4b8d169d804a0ae4f2943ff
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-29 17:01:42 -0700

    file 3-strcmp.c content change 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	rtrn_value = 0;
 
-	while ((*s1 != '\0' || *s2 != '\0') && rtrn_value == 0)
+	while (s1[i] != '\0' && s2[i] != '\0' && rtrn_value == 0)
 	{
 		if (s1[i] > s2[i])
 		{

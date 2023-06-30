0dfc71670d2e6906229a70a3858fa8557cf5407e
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-06-29 18:13:54 -0700

    Task 5. Always look up answered

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * @s: a string
+ *
+ * Return: the address or s
+ */
+char *string_toupper(char *s)
+{
+	char *ptr;
+	int i;
+
+	ptr = s;
+	while (*s != '\0')
+	{
+		if (*s > 'a' && *s < 'z')
+		{
+			*s = *s - 32;
+		}
+		i++;
+		s++;
+	}
+
+	return (ptr);
+}

24dc0790a269ebd67413efb0cf4db1657cc01847
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-03 16:09:09 -0700

    the making of 0x07-pointers_arrays_strings directory and it is README.md

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,52 +13,45 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	/* Get lengths of input strings */
 	int len1 = 0;
+	int len2 = 0;
+	int result_len = len1 > len2 ? len1 + 1 : len2 + 1;
+	int carry = 0;
+	int i, j, k, digit1, digit2, digit_sum;
+	int start_index = 0;
+
 	while (n1[len1] != '\0')
 	{
 		len1++;
 	}
-	int len2 = 0;
 	while (n2[len2] != '\0')
 	{
 		len2++;
 	}
-
-	/* Determine the length of the result and check if it fits in the output buffer */
-	int result_len = len1 > len2 ? len1 + 1 : len2 + 1;
 	if (result_len > size_r)
 	{
-		return 0;
+		return (0);
 	}
-
-	/* Initialize the result buffer and carry variable */
-	int carry = 0;
-	int i, j, k;
 	for (i = len1 - 1, j = len2 - 1, k = result_len - 1; k >= 0; i--, j--, k--)
 	{
-		int digit1 = i >= 0 ? n1[i] - '0' : 0;
-		int digit2 = j >= 0 ? n2[j] - '0' : 0;
-		int digit_sum = digit1 + digit2 + carry;
+		digit1 = i >= 0 ? n1[i] - '0' : 0;
+		digit2 = j >= 0 ? n2[j] - '0' : 0;
+		digit_sum = digit1 + digit2 + carry;
 		carry = digit_sum >= 10 ? 1 : 0;
 		r[k] = digit_sum % 10 + '0';
 	}
-
-	/* Remove leading zeros from the result */
-	int start_index = 0;
 	while (r[start_index] == '0' && start_index < result_len - 1)
 	{
 		start_index++;
 	}
-
-	/* Shift the result to the beginning of the output buffer */
 	if (start_index > 0)
 	{
 		result_len -= start_index;
-		memmove(r, r + start_index, result_len);
+		for (i = 0; i < result_len; i++)
+		{
+			r[i] = r[i + start_index];
+		}
 	}
-
-	/* Terminate the result with a null character and return a pointer to it */
 	r[result_len] = '\0';
-	return r;
+	return (r);
 }

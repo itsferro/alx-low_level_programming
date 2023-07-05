526eb300c5bab2913f3a2d4066adf21712ad2dfa
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-05 14:15:02 -0700

    Task 0 answered

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: a string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}

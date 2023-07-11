9077d7c0834bf83e70811b7cc7be023fe3aa5d06
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-11 16:50:58 -0700

    Task number 2 attempt number 4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -42,16 +42,15 @@ char *str_concat(char *s1, char *s2)
 	char *new_string;
 	int i, n;
 
-	s1_len = _strlen(s1);
+	s1_len = _strlen(s1) - 1;
 	s2_len = _strlen(s2);
-	printf("s1_len = %d\ns2_len = %d\n", s1_len, s2_len);
 
-	/*if (s1 == NULL)
+	if (s1 == NULL)
 		s1_len = 0;
 	if (s2 == NULL)
 		s2_len = 0;
-*/
-	new_string = (char *) malloc((s1_len + s2_len - 1) * sizeof(char));
+
+	new_string = (char *) malloc((s1_len + s2_len) * sizeof(char));
 
 	if (new_string == NULL)
 		return (NULL);

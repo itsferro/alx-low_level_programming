a31fd221892be197c595006212a189aa0e5020f0
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-11 19:10:08 -0700

    Task number 3 attempt number 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,9 +32,11 @@ int **alloc_grid(int width, int height)
 		if (result[i] == NULL)
 		{
 			for (i = 0; i < height; i++)
+			{
 				free(result[i]);
-			free(result);
-			return (NULL);
+				free(result);
+			}
+				return (NULL);
 		}
 		for (c = 0; c < width; c++)
 		{

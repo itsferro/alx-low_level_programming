6d6b9d5152ebc41a06198418fd12656d6039e046
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-13 17:31:08 -0700

    Task number 2 attempt 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,10 +16,10 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
+	void *array;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
 	array = malloc(nmemb * size);
@@ -28,7 +28,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i <= nmemb; i++)
-		array[i] = 0;
+		*((char *)array + i) = 0;
 
 	return (array);
 }

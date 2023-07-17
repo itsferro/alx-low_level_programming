3ea962efefd6cb71b15b64b0e8b3de55fc4c7b63
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-17 16:26:10 -0700

    Task number 0 attempt number 2

--- 0-object_like_macro.h
+++ 0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0-object_like_macro
-#define 0-object_like_macro
+#ifndef object_like_macro
+#define object_like_macro
 
 #define SIZE 1024
 
-#endif //the ind of #ifndef 0-object_like_macro
+#endif /*the ind of #ifndef 0-object_like_macro*/

c2b79052f7ee3868958145e91fe3749b4d68e3fd
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-18 16:25:36 -0700

    Task number 3 attempt number 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,14 +12,14 @@
  *		holding the dog information.
  */
 
-typedef struct dog dog_t;
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 

f10ab27dcbbfb6abd8afbca6f3c1e1e29775b5d3
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-20 11:11:38 -0700

    Task number 2 attempt 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -26,5 +26,7 @@ char *_strstr(char *haystack, char *needle);
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
 
 #endif

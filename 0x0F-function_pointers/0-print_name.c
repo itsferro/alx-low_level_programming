f73ae805516ba3c173965d81d1f5e8d7b3d3bf76
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-20 10:06:26 -0700

    Task number 0 attempt number 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name.
+ *
+ * @name: a string.
+ * @f:a pointer to a function.
+ *
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}

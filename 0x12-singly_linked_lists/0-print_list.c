95832e2830a8ae3db4a8264217e70644eb4f58d7
Feras Alzaidi <feras.mamon.alzaidi@gmail.com>
2023-07-27 12:14:31 -0700

    Task number 0 attempt number 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ *
+ * @h: a constant pointer of type list_t.
+ *
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr = h;
+	int count = 0;
+
+	if (h == NULL)
+		printf("Linked list is empty");
+
+	while (ptr != NULL)
+	{
+		count++;
+		if (ptr->str != NULL)
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		else
+			printf("[0] (nil)\n");
+		ptr = ptr->next;
+	}
+	return (count);
+}

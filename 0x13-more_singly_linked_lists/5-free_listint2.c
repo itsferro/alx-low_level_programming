49ac4e43449c7af4853754846b98249e994120a0
itsferro <feras.mamon.alzaidi@gmail.com>
2023-08-01 15:23:02 -0700

    Task number 5 attempt number 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,6 +11,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current_node, *next_node;
 
+	if (head == NULL)
+		return;
+
 	current_node = *head;
 	*head = NULL;
 	while (current_node != NULL)

f1e4511c0fd16cd1dfb5c3ab6fb2da80e4ca6d0a
itsferro <feras.mamon.alzaidi@gmail.com>
2023-08-01 14:58:57 -0700

    Task number 5 attempt number 1

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ *
+ * @head: a pointer of type listint_t.
+ *
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node, *next_node;
+
+	current_node = *head;
+	*head = NULL;
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
+}

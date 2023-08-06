b8443ae8e023f8d43328d725988d644fa0fac95e
itsferro <feras.mamon.alzaidi@gmail.com>
2023-08-06 16:59:53 -0700

    Task number 1 attempt number 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,16 +9,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int bit_count, size, mask = 1;
-
-	size = (sizeof(unsigned long int)) * 4;
-	/*printf("-----------------------\n%ld\t%ld\n", size, mask);*/
-	mask = mask << size;
-	/*printf("%ld\t%ld\n\n", size, mask);*/
-
-	for (bit_count = 0; bit_count <= size; bit_count++)
-	{
-		_putchar(n & mask ? '1' : '0');
-		n <<= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar(n & 1 ? '1' : '0');
 }

f23289f563b4dec0366381315c87a99295a9a32f
itsferro <feras.mamon.alzaidi@gmail.com>
2023-08-08 18:50:53 -0700

    Task number 0 attempt number 1

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /*the end of #ifndef MAIN_H*/

6d994b80b21a7c2f5bd2258af2d99c41f55ba54b
itsferro <feras.mamon.alzaidi@gmail.com>
2023-08-08 19:26:36 -0700

    Task number 1 attempt number 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor = 0;
+	size_t content_length, bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		content_length = strlen(text_content);
+		bytes_written = write(file_descriptor, text_content, content_length);
+		if (bytes_written != content_length)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}

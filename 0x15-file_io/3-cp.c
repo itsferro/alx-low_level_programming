c7cb805de2683f35c84f4610b1176013207377ab
itsferro <feras.mamon.alzaidi@gmail.com>
2023-10-21 04:04:24 -0700

    task number 3 attempt number 2 (betty fixed)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,9 +10,9 @@
 /**
  * print_error - prints an error to the stderr.
  *
- * message: the ,essage to print.
- * file_name: the name of the file related to the error.
- * exit_code: the code provided on exit for debugging.
+ * @message: the ,essage to print.
+ * @file_name: the name of the file related to the error.
+ * @exit_code: the code provided on exit for debugging.
  */
 void print_error(const char *message, const char *file_name, int exit_code)
 {
@@ -23,8 +23,8 @@ void print_error(const char *message, const char *file_name, int exit_code)
 /**
  * main - copies the content of a file to another file.
  *
- * argc: is the arguments count.
- * argv: is the arguments list.
+ * @argc: is the arguments count.
+ * @argv: is the arguments list.
  *
  * Return:
  *	97 if the number of argument is not the correct one.
@@ -39,47 +39,35 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
-	{
 		print_error("Usage: cp file_from file_to", "", 97);
-	}
 
 	file_from = argv[1];
 	file_to = argv[2];
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-	{
 		print_error("Can't read from file", file_from, 98);
-	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (fd_to == -1)
-	{
 		print_error("Can't write to", file_to, 99);
-	}
 
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1) {
+		if (bytes_written == -1)
 			print_error("Can't write to", file_to, 99);
-		}
 	}
 
 	if (bytes_read == -1)
-	{
 		print_error("Can't read from file", file_from, 98);
-	}
 
 	if (close(fd_from) == -1)
-	{
 		print_error("Can't close fd", file_from, 100);
-	}
 
 	if (close(fd_to) == -1)
-	{
 		print_error("Can't close fd", file_to, 100);
-	}
 
-	return 0;
+	return (0);
 }

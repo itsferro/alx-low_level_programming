b2e0dd27503e2135dcff2e5711affe752b69bb77
itsferro <feras.mamon.alzaidi@gmail.com>
2023-10-30 17:28:35 -0700

    project number 0x15 answered

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ *
+ * @filename: the file name string.
+ * @text_content: a string holds the text
+ *	that will be written in  to the file.
+ *
+ * Return: 1 on success, -1 on failure
+ *	(file can not be created, file can not be written,
+ *	write “fails”, etc…)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor = 0;
+	size_t content_length, bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename,
+			O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		content_length = strlen(text_content);
+		bytes_written = write(file_descriptor, text_content, content_length);
+		if (bytes_written != content_length)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * print_error - prints an error to the stderr.
+ *
+ * @message: the ,essage to print.
+ * @file_name: the name of the file related to the error.
+ * @exit_code: the code provided on exit for debugging.
+ */
+void print_error(const char *message, const char *file_name, int exit_code)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", message, file_name);
+	exit(exit_code);
+}
+
+/**
+ * main - copies the content of a file to another file.
+ *
+ * @argc: is the arguments count.
+ * @argv: is the arguments list.
+ *
+ * Return:
+ *	97 if the number of argument is not the correct one.
+ *	98 if file_from does not exist, or if you can not read it.
+ *	99 if you can not create or if write to file_to fails.
+ *	100 if you can not close a file descriptor.
+ */
+int main(int argc, char *argv[])
+{
+	char *file_from, *file_to, buffer[BUFFER_SIZE];
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+
+	if (argc != 3)
+		print_error("Usage: cp file_from file_to", "", 97);
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		print_error("Error: Can't read from file", file_from, 98);
+	
+	if (access(file_to, F_OK) == 0)
+	{
+		fd_to = open(file_to, O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		if (fd_to == -1)
+			print_error("Error: Can't write to", file_to, 99);
+	}
+	else
+	{
+		fd_to = open(file_to, O_WRONLY | O_CREAT);
+		if (fd_to == -1)
+			print_error("Error: Can't write to", file_to, 99);
+		chmod(file_to, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	}
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+			print_error("Error: Can't write to", file_to, 99);
+	}
+
+	if (bytes_read == -1)
+		print_error("Error: Can't read from file", file_from, 98);
+
+	if (close(fd_from) == -1)
+		print_error("Error: Can't close fd", file_from, 100);
+
+	if (close(fd_to) == -1)
+		print_error("Error: Can't close fd", file_to, 100);
+
+	return (0);
+}

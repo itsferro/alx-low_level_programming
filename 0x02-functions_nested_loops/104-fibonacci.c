91a117083a1f26fde0bb955a6ac3d135f459fce7
itsferro <feras.mamon.alzaidi@gmail.com>
2023-12-23 04:18:42 -0800

    Task numbwe 104 attempt number 4 after a long time

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,45 @@
  */
 int main(void)
 {
-	long double pn1, pn2, vh;
-	int i;
+	long int pn1, pn2, sum, sum1, sum2;
+	long int var11, var12, var21, var22;
+	int i, c;
 
 	pn2 = 0;
 	pn1 = 1;
 
 	for (i = 0; i < 98; i++)
 	{
-		vh = pn1;
-		pn1 = pn1 + pn2;
-		pn2 = vh;
-		if (i < 97)
+		if (i < 91)
 		{
-			printf("%.Lf, ", pn1);
+			sum = pn1 + pn2;
+			pn1 = pn2;
+			pn2 = sum;
+			printf("%lu, ", sum);
 		}
 		else
 		{
-			printf("%.Lf", pn1);
+			if (i == 91)
+			{
+				var11 = pn1 / 1000000000;
+				var12 = pn1 % 1000000000;
+				var21 = pn2 / 1000000000;
+				var22 = pn2 % 1000000000;
+			}
+			else
+			{
+				var11 = var21;
+				var12 = var22;
+				var21 = sum1;
+				var22 = sum2;
+			}
+			sum2 = (var12 + var22) % 1000000000;
+			c = (var12 + var22) / 1000000000;
+			sum1 = var11 + var21 + c;
+			if (!(i == 97))
+				printf("%lu%lu, ", sum1, sum2);
+			else
+				printf("%lu%lu", sum1, sum2);
 		}
 	}
 	printf("\n");

1b8ee03491c5c4b5a655d40e8550a9a5ba9fd968
itsferro <feras.mamon.alzaidi@gmail.com>
2024-04-11 19:56:31 -0700

    Task number 11 attempt number 2

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -2,10 +2,11 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - searches for a value in a sorted array of integers.
+ * advanced_binary_logic - searches for a value in a sorted array of integers.
  *
  * @array: an ascending ordered array.
- * @size: the number if elements in ther array.
+ * @left: the start of the array.
+ * @right: the end of the array.
  * @value: the value to search for.
  *
  * Return:
@@ -13,34 +14,44 @@
  *	-1 If value is not present in array
  *	or if array is NULL.
  */
+int advanced_binary_logic(int *array, size_t left, size_t right, int value)
+{
+	size_t mid, i;
+
+	if (right < left)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%u, ", array[i]);
+	printf("%u\n", array[i]);
+
+	mid = left + (right - left) / 2;
+
+	if (array[mid] == value && (mid == left || array[mid - 1] != value))
+		return (i);
+	if (array[mid] >= value)
+		return (advanced_binary_logic(array, left, mid, value));
+	return (advanced_binary_logic(array, (mid + 1), right, value));
+
+}
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers.
+ *
+ * @array: an ascending ordered array.
+ * @size: the number if elements in ther array.
+ * @value: the value to search for.
+ *
+ * Return:
+ *      the index where value is located.
+ *      -1 If value is not present in array
+ *      or if array is NULL.
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t left, right, mid, i, result = -1;
+	if (array == NULL || size < 1)
+		return (-1);
 
-	left = 0;
-	right = size - 1;
-
-	while (left <= right)
-	{	mid = left + (right - left) / 2;
-
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%u, ", array[i]);
-		printf("%u\n", array[i]);
-
-		if (array[mid] == value && (mid == left || array[mid - 1] != value))
-		{
-			result = mid;
-			right = mid - 1;
-		}
-		else if (array[mid] >= value)
-		{
-			right = mid;
-		}
-		else
-		{
-			left = mid + 1;
-		}
-	}
-	return (result);
+	return (advanced_binary_logic(array, 0, size - 1, value));
 }

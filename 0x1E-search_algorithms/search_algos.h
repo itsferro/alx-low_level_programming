306ae74f00d7322660c9ef2705290b7f034eac16
itsferro <feras.mamon.alzaidi@gmail.com>
2024-04-14 17:49:49 -0700

    Task number 0 attempt number 3

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,8 @@ int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t r);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-size_t calculate_index(size_t index_l, size_t index_h, int l, int h, int value);
+size_t calculate_index(size_t index_l,
+		size_t index_h, int l, int h, int value);
 int interpolation_search(int *array, size_t size, int value);
 size_t binary_search_2(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
